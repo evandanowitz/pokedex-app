2d9935bc5fce5c69dbbfccda80367322cd624c84
evandanowitz <139590465+evandanowitz@users.noreply.github.com>
2023-08-02 17:59:05 -0400

    Add three objects to array

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,20 @@
 // Creates an empty array
 let pokemonList = [1, 2, 3];
+
+pokemonList[0] = {
+    name: 'Pikachu',
+    height: 1.04,
+    type: 'Electric'
+};
+
+pokemonList[1] = {
+    name: 'Bulbasaur',
+    height: 2.04,
+    type: ['Grass','Poison']
+};
+
+pokemonList[2] = {
+    name: 'Charizard',
+    height: 5.07,
+    type: ['Fire','Flying']
+};
