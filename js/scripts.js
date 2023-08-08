6eff14e1eabb09aeeb27e8de947af82707ed3d73
evandanowitz <139590465+evandanowitz@users.noreply.github.com>
2023-08-08 09:47:18 -0400

    Simplify array and for loop
    Remove separate objects causing redundancy. Update all objects in array instead.
    
    Combine for loops causing redundancy into one for loop.

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,12 @@
 // Creates an empty array
 let pokemonList = [
-    {name: 'Pikachu'},
-    {name: 'Bulbasaur'},
-    {name: 'Charizard'}
+    {name: 'Pikachu', height: 1, types: ['Electric']},
+    {name: 'Bulbasaur', height: 2, types: ['Grass','Poison']},
+    {name: 'Charizard', height: 5, types: ['Fire','Flying']}
 ];
-
-// Create object using specific index
-pokemonList[0] = {
-    name: 'Pikachu',
-    height: 1,
-    types: ['Electric']
-};
-// Create object using specific index
-pokemonList[1] = {
-    name: 'Bulbasaur',
-    height: 2,
-    types: ['Grass','Poison']
-};
-// Create object using specific index
-pokemonList[2] = {
-    name: 'Charizard',
-    height: 5,
-    types: ['Fire','Flying']
-};
 
 for (let i = 0; i < pokemonList.length; i++) {
     if (pokemonList[i]) {
-        document.write([pokemonList[i].name] + ' (height: ' + [pokemonList[i].height] + ')<br><br>')
-    }
-    if (pokemonList[i].height > 3) {
-        document.write([pokemonList[i].name] + ' (height: ' + [pokemonList[i].height] + ')' + ' - Wow, that\'s big!')
+        document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')' + (pokemonList[i].height > 3 ? ' - Wow, that\'s big!' : '') + '<br><br>');
     }
 }
